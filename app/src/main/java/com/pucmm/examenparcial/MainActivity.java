6555282dd9c7e870edd16421f1e318616ac1a7e9
Joseaquilesm <33287719+Joseaquilesm@users.noreply.github.com>
2020-10-19 23:07:27 -0400

    initial commit

---
+++ app/src/main/java/com/pucmm/examenparcial/MainActivity.java
@@ -0,0 +1,32 @@
+package com.pucmm.examenparcial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    //List view stuff
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.listView1);
+
+    }
+    public void mostrarInfo(){
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+    }
+}
